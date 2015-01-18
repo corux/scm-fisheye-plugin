0a77a15e741c3a625aeed1a5e476829dc0697484
Tobias Wallura <tobias@wallura.org>
2015-01-18 10:32:59 +0100

    fix typo and reformat js files

--- src/main/resources/scm/corux.fisheye.globalconfigpanel.js
+++ src/main/resources/scm/corux.fisheye.globalconfigpanel.js
@@ -1,115 +1,111 @@
 Ext.ns("corux.fisheye");
 
-corux.fisheye.LinkWindow = Ext
-    .extend(
-        Ext.Window,
-        {
-          titleText : 'Link repositories',
-          okText : 'Ok',
-          cancelText : 'Cancel',
-          connectingText : 'Connecting',
-          statusText : 'Status',
-          failedText : 'linking repositories failed!',
-          successText : 'Repositories linked successfully.',
-          waitMsgText : 'Sending data...',
+corux.fisheye.LinkWindow = Ext.extend(Ext.Window, {
+  titleText : 'Link repositories',
+  okText : 'Ok',
+  cancelText : 'Cancel',
+  connectingText : 'Connecting',
+  statusText : 'Status',
+  failedText : 'linking repositories failed!',
+  successText : 'Repositories linked successfully.',
+  waitMsgText : 'Sending data...',
 
-          usernameText : 'Username',
-          usernameHelpText : 'Username used to retrieve available repositories from the fisheye server.',
+  usernameText : 'Username',
+  usernameHelpText : 'Username used to retrieve available repositories from the fisheye server.',
 
-          passwordText : 'Password',
-          passwordHelpText : 'Password used to retrieve available repositories from the fisheye server.',
+  passwordText : 'Password',
+  passwordHelpText : 'Password used to retrieve available repositories from the fisheye server.',
 
-          helpText : 'Retrieves all available repositories from fisheye and links them to the SCM repositories',
+  helpText : 'Retrieves all available repositories from fisheye and links them to the SCM repositories',
 
-          initComponent : function() {
-            var config = {
-              layout : 'fit',
-              width : 300,
-              height : 170,
-              closable : false,
-              resizable : false,
-              plain : true,
-              border : false,
-              modal : true,
-              title : this.titleText,
-              items : [ {
-                id : 'linkRepositoryForm',
-                url : restUrl + 'plugins/fisheye/link.json',
-                frame : true,
-                xtype : 'form',
-                monitorValid : true,
-                defaultType : 'textfield',
-                items : [ {
-                  name : 'username',
-                  fieldLabel : this.usernameText,
-                  helpText : this.usernameHelpText,
-                  allowBlank : false
-                }, {
-                  name : 'password',
-                  fieldLabel : this.passwordText,
-                  helpText : this.passwordHelpText,
-                  inputType : 'password',
-                  allowBlank : false
-                }, {
-                  xtype : 'panel',
-                  html : this.helpText
-                } ],
-                buttons : [ {
-                  text : this.okText,
-                  formBind : true,
-                  scope : this,
-                  handler : this.linkRepositories
-                }, {
-                  text : this.cancelText,
-                  scope : this,
-                  handler : this.cancel
-                } ]
-              } ]
-            };
+  initComponent : function() {
+    var config = {
+      layout : 'fit',
+      width : 300,
+      height : 170,
+      closable : false,
+      resizable : false,
+      plain : true,
+      border : false,
+      modal : true,
+      title : this.titleText,
+      items : [ {
+        id : 'linkRepositoryForm',
+        url : restUrl + 'plugins/fisheye/link.json',
+        frame : true,
+        xtype : 'form',
+        monitorValid : true,
+        defaultType : 'textfield',
+        items : [ {
+          name : 'username',
+          fieldLabel : this.usernameText,
+          helpText : this.usernameHelpText,
+          allowBlank : false
+        }, {
+          name : 'password',
+          fieldLabel : this.passwordText,
+          helpText : this.passwordHelpText,
+          inputType : 'password',
+          allowBlank : false
+        }, {
+          xtype : 'panel',
+          html : this.helpText
+        } ],
+        buttons : [ {
+          text : this.okText,
+          formBind : true,
+          scope : this,
+          handler : this.linkRepositories
+        }, {
+          text : this.cancelText,
+          scope : this,
+          handler : this.cancel
+        } ]
+      } ]
+    };
 
-            Ext.apply(this, Ext.apply(this.initialConfig, config));
-            Sonia.action.ChangePasswordWindow.superclass.initComponent.apply(
-                this, arguments);
-          },
+    Ext.apply(this, Ext.apply(this.initialConfig, config));
+    Sonia.action.ChangePasswordWindow.superclass.initComponent.apply(this, arguments);
+  },
 
-          linkRepositories : function() {
-            var form = Ext.getCmp('linkRepositoryForm').getForm();
-            form.submit({
-              scope : this,
-              method : 'POST',
-              waitTitle : this.connectingText,
-              waitMsg : this.waitMsgText,
-              success : function() {
-                if (debug) {
-                  console.debug(this.successText);
-                }
-                Ext.MessageBox.show({
-                  title : this.statusTitle,
-                  msg : this.successText,
-                  buttons : Ext.MessageBox.OK,
-                  icon : Ext.MessageBox.INFO
-                });
-                this.close();
-              },
-
-              failure : function() {
-                if (debug) {
-                  console.debug(this.failedText);
-                }
-                Ext.MessageBox.show({
-                  title : this.statusTitle,
-                  msg : this.failedText,
-                  buttons : Ext.MessageBox.OK,
-                  icon : Ext.MessageBox.ERROR
-                });
-              }
-            });
-          },
-
-          cancel : function() {
-            this.close();
-          }
+  linkRepositories : function() {
+    var form = Ext.getCmp('linkRepositoryForm').getForm();
+    form.submit({
+      scope : this,
+      method : 'POST',
+      waitTitle : this.connectingText,
+      waitMsg : this.waitMsgText,
+      success : function() {
+        if (debug) {
+          console.debug(this.successText);
+        }
+        Ext.MessageBox.show({
+          title : this.statusText,
+          msg : this.successText,
+          buttons : Ext.MessageBox.OK,
+          icon : Ext.MessageBox.INFO
         });
+        this.close();
+      },
+
+      failure : function() {
+        if (debug) {
+          console.debug(this.failedText);
+        }
+        Ext.MessageBox.show({
+          title : this.statusText,
+          msg : this.failedText,
+          buttons : Ext.MessageBox.OK,
+          icon : Ext.MessageBox.ERROR
+        });
+      }
+    });
+  },
+
+  cancel : function() {
+    this.close();
+  }
+});
 
 corux.fisheye.GlobalConfigPanel = Ext
     .extend(
@@ -167,8 +163,7 @@ corux.fisheye.GlobalConfigPanel = Ext
             }
 
             Ext.apply(this, Ext.apply(this.initialConfig, config));
-            corux.fisheye.GlobalConfigPanel.superclass.initComponent.apply(
-                this, arguments);
+            corux.fisheye.GlobalConfigPanel.superclass.initComponent.apply(this, arguments);
           },
 
           onSubmit : function(values) {
